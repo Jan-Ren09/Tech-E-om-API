93c2e56b9c8f010b456952801bac584a4cf723e4
Jan-Ren09 <janren.delasalas@gmail.com>
2024-09-04 17:13:33 +0800

    added fix typo

--- controllers/user.js
+++ controllers/user.js
@@ -84,7 +84,7 @@ module.exports.loginUser = (req, res) => {
 			//User does not exist, return false
 			if(result == null) {
 				// Send status 404
-				return res.status(404).send({ error: 'No email found' });
+				return res.status(401).send({ error: 'No email found' });
 
 			//User exists		
 			} else {
