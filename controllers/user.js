73eff8b64ed9bc59d18fe586cfc688e71ef58685
Jan-Ren09 <janren.delasalas@gmail.com>
2024-09-04 15:28:05 +0800

    Typos

--- controllers/user.js
+++ controllers/user.js
@@ -172,7 +172,7 @@ module.exports.makeUserAdmin = async (req, res) => {
         const user = await User.findById(req.params.id);
 
         if (!user) {
-            return res.status(404).json({ message: "User doesn't exist" });
+            return res.status(404).json({ message: "User not found" });
         }
 
         // Update the user's role to admin
