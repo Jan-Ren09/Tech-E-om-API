67a49ea3aa6e4076aed6341df642f543bcf7f019
Jan-Ren09 <janren.delasalas@gmail.com>
2024-09-05 14:38:41 +0800

    Debug

--- controllers/cart.js
+++ controllers/cart.js
@@ -129,7 +129,7 @@ module.exports.updateProductQuantity = async (req, res) => {
         return res.status(404).send({ message: 'Product not found' });
       }
       
-      item.subtotal = product.price * newQuantity;
+      item.subtotal = product.price * quantity;
     }
     
     cart.totalPrice = cart.cartItems.reduce((acc, item) => acc + item.subtotal, 0);
