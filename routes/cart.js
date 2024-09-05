c9041ca37326694417c090dc144ee10353b5666e
Jan-Ren09 <janren.delasalas@gmail.com>
2024-09-05 16:10:10 +0800

    Changed routes

--- routes/cart.js
+++ routes/cart.js
@@ -4,10 +4,10 @@ const {verify, verifyAdmin} = require("../auth");
 const router = express.Router();
 
 // Get cart
-router.get('/add-to-cart', verify, cartController.getCart);
+router.get('/get-cart', verify, cartController.getCart);
 
 // Add to cart
-router.post('/add-to-cart', verify, cartController.addToCart);
+router.get('/add-to-cart', verify, cartController.addToCart);
 
 // Change product quantity
 router.patch('/update-cart-quantity', verify, cartController.updateProductQuantity);
@@ -16,6 +16,6 @@ router.patch('/update-cart-quantity', verify, cartController.updateProductQuanti
 router.patch('/:productId/remove-from-cart', verify, cartController.removeProduct);
 
 // Clear cart
-router.put('/clear-cart', verify, cartController.clearCart);
+router.post('/clear-cart', verify, cartController.clearCart);
 
 module.exports = router;
--- routes/order.js
+++ routes/order.js
@@ -7,7 +7,7 @@ router.post('/checkout', verify, orderController.createOrder);
 
 router.get('/all-orders', verify, verifyAdmin, orderController.getAllOrders);
 
-router.get('/my-orders', verify, orderController.getUserOrders);
+router.post('/my-orders', verify, orderController.getUserOrders);
 
 
 
