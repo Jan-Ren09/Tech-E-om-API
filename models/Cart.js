73ea8392b127cd05eb7be7ace5dba9d1b8298d28
Jan-Ren09 <janren.delasalas@gmail.com>
2024-09-25 18:20:39 +0800

    Added Image

--- models/Cart.js
+++ models/Cart.js
@@ -23,7 +23,6 @@ const cartSchema = new mongoose.Schema({
             },
             image: { 
                 type: String,
-                required: [true, 'Image URL is required'] 
             }
         }
     ],
