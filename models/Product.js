2958c7f14b1e21e24d2b0e2f7d495d4b5b6c1605
Jan-Ren09 <janren.delasalas@gmail.com>
2024-09-25 14:56:18 +0800

    Model changes

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,10 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Name is required']
     },
+    description: {
+        type: String,
+        required: [true, 'Course Description is Required']
+    },
     price: {
         type: Number,
         required: [true, 'Course Price is Required']
